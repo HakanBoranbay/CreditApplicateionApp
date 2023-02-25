0e6f7ee685ed92d3edfb5c365d78f7ab6a5ede22
Hakan Boranbay <boranbayhakan@gmail.com>
2023-02-26 01:04:30 +0300

    Kafka logger configuration added.

--- creditapp/src/main/java/com/hakanboranbay/creditapp/controller/CreditApplicationController.java
+++ creditapp/src/main/java/com/hakanboranbay/creditapp/controller/CreditApplicationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,8 @@ public class CreditApplicationController {
     private CreditService creditService;
     @Autowired
     private ClientService clientService;
+    @Autowired 
+    KafkaTemplate<String, String> producer;
 
     @PostMapping("/credits")
     public ResponseEntity<?> create(@RequestBody CreditApplicationRequest request) {
